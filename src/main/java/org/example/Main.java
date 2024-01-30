b737cf75623a43bfcd1025d60e7db866ab55264a
rejme <git@github.com:liono2005/PR1.git>
2024-01-30 21:11:54 +0300

    rgrgdfdf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     //5. Методы
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        double result = triangle(a, b, c);
+        System.out.println("Результат: " + result);
     }
-}
+    private static double triangle(int a, int b, int c) {
+        int p = (a + b + c);
+        return p;
+    }
+    }
